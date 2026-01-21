b3f5c68233485a70cd6392a0504178babe96690f
leo <leo@debian.myguest.virtualbox.org>
2026-01-21 16:33:13 -0600

    Renamed .sh, fixed jq error message

--- har-ip-extract.sh
+++ har-ip-extract.sh
@@ -2,8 +2,8 @@
 
 if [[ "$#" -eq 0 || ! -f "$1" ]]; then
 	printf "\nUsage: %s [FILE]\n" "$(basename "$0")"
-	printf "=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~==~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~="
-	printf "\nThis program accepts a .har (HTTP Archive file) as input, and outputs extracted IPs and redirects.\n"
+	printf "=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~==~=~=~=~=~=~=~="
+	printf "\nThis program accepts a .har (HTTP Archive file) as input, and outputs extracted IPs and redirects.\n\n"
 	exit 1
 fi
 
@@ -11,10 +11,10 @@ FILE=$1
 MATCH=""
 
 if command -v jq >/dev/null 2>&1; then
-	printf "\n~=~=~=~ Extracting redirects and URL for domains(): =~=~=~\n"
+	printf "\n~=~=~=~ Extracting redirects and URL for domains(): ~=~=~=~\n"
 	jq -r '.log.pages[] | select (.title) | .title' "$FILE"
 
-	printf "\n~=~=~=~ Redirects and IPs: =~=~=~\n"	
+	printf "\n~=~=~=~ Redirects and IPs: ~=~=~=~\n"	
 	MATCH=$(jq -r '.log.entries[] | select (.response.redirectURL != "") | .response.redirectURL' "$FILE" | sed 's/https*:\/\///g; s/\/.*//g' | sort -u)
 	for line in $MATCH; do
 		if [[ $line == "www."* ]]; then
@@ -26,6 +26,6 @@ if command -v jq >/dev/null 2>&1; then
 	jq -r '.log.entries[] | select (.serverIPAddress  != null) | .serverIPAddress' "$FILE" | sort -u
 	exit 0
 else
-	printf"\njq is not installed. This program requires jq to run.\nTo install jq, run the command "pt-get install jq.""
+	printf "\njq is not installed. This program requires jq to run.\n\nTo install jq, run the command 'apt-get install jq.'\n\n"
 	exit 1
 fi
